fa702afbe479fbb7907671beb1f8c82fe87217eb
Donna Mwiine <dmwiine@gmail.com>
2017-04-18 16:15:05 +0300

    Added car class

---
+++ car_class.py
@@ -0,0 +1,34 @@
+class Car(object):
+	def __init__(self,typ = "saloon", model = 'GM', name = 'General'):
+		self.type = typ
+		self.model = model
+		self.name = name
+		
+	def car_doors(self)
+		if self.name == 'Koenigsegg' or self.name =='Porshe':
+			return 2
+		else:
+			return 4
+
+	def car_wheels(self)
+		if self.type == 'trailer':
+			return 8
+		else:
+			return 4
+	def is_saloon(self):
+		if self.type == "saloon":
+			return True
+		else:
+			return False
+
+	def drive_car(self,x):
+		if x == 3:
+            self.speed = 1000
+
+        elif x == 7:
+            self.speed = 77
+        else:
+            self.speed = 56
+        return self
+
+
